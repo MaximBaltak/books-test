1d9868e64039cfe037242e1e2371c65c1f345276
Your Name <you@example.com>
2025-08-25 11:51:58 +0200

    переделал на 1 запрос

--- controllers/books-route.js
+++ controllers/books-route.js
@@ -7,26 +7,80 @@ router.get('/', async (req, res) => {
     const limit = 5; 
     const skip = (page - 1) * limit;
 
-    const totalBooks = await Book.countDocuments();
+   const result = await Book.aggregate([
+    {
+      $lookup: {
+        from: 'authors',
+        localField: 'author',
+        foreignField: '_id',
+        as: 'author'
+      }
+    },
 
-    const books = await Book.find()
-      .populate('author', 'name')
-      .skip(skip)
-      .limit(limit);
+    {
+      $addFields: {
+        author: {
+          $cond: {
+            if: { $eq: [{ $size: '$author' }, 0] },
+            then: null,
+            else: { $arrayElemAt: ['$author', 0] }
+          }
+        }
+      }
+    },
 
-    const booksFormat = books.map(book => ({
-      id: book._id,
-      title: book.title,
-      author: book.author?.name || null,
-    }));
+    {
+      $addFields: {
+        author: '$author.name'  
+      }
+    },
+    {
+      $addFields: {
+        id: '$_id',       
+        author: {        
+          $ifNull: ['$author', null]
+        }
+      }
+    },
+    {
+      $project: {
+        _id: 0,              
+        __v: 0          
+      }
+    },
+    {
+      $facet: {
+          metadata: [
+          { $count: "total" }
+        ],
+          data: [
+          { $skip: skip },
+          { $limit: limit }
+        ]
+      }
+    },
 
-    const totalPages = Math.ceil(totalBooks / limit);
+    {
+      $project: {
+        data: 1,
+        totalCount: { $arrayElemAt: ['$metadata.total', 0] },
+        pagesCount: {
+          $ceil: { $divide: [{ $arrayElemAt: ['$metadata.total', 0] }, limit] }
+        }
+      }
+    }
+  ]);
 
-    res.json({
-      books: booksFormat,
-      pagesCount: totalPages,
-      totalCount: totalBooks,
-    });
+  const books = result[0]?.data || [];
+  const totalCount = result[0]?.totalCount || 0;
+  const pagesCount = result[0]?.pagesCount || 0;
+
+  res.json({
+    books,
+    totalCount,
+    pagesCount
+  });
+    
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
